834560caeb53a213ccb6434bf6f4b159d1ebc5a5
u809748758 <u809748758@juanpablo.tk>
2018-06-04 14:30:26 -0430

    Jua here

--- php/process.php
+++ php/process.php
@@ -15,6 +15,9 @@
   <br>
   <button type="button" name="button" onclick="location.href='../';">Volver</button>
   </form>
+</body>
+</html>
+
   <?php
   include '../vendor/autoload.php';
   use GuzzleHttp\Client;
@@ -25,29 +28,45 @@
     $client = new Client([
       'base_uri' => 'https://api-codigos-postales.herokuapp.com/v2/',
     ]);
-    // Josn Response
+    // Json Response
     $response_cp1 = $client->request('GET','codigo_postal/'.$cp1);
     $response_cp2 = $client->request('GET','codigo_postal/'.$cp2);
     // PHP Object Response
     $response_cp1_object = json_decode($response_cp1->getBody());
     $response_cp2_object = json_decode($response_cp2->getBody());
 
-    echo '<ul>
+    /*   DISTANCE */
+
+    $client_dis = new Client([
+      'base_uri' => 'https://maps.googleapis.com/maps/api/distancematrix/',
+    ]);
+    //Json Response
+    $tourl_string_municipio_1 = str_replace('', '+', $response_cp1_object->municipio);
+    $tourl_string_estado_1 = str_replace('', '+', $response_cp1_object->estado);
+
+    $tourl_string_municipio_2 = str_replace('', '+', $response_cp2_object->municipio);
+    $tourl_string_estado_2 = str_replace('', '+', $response_cp2_object->estado);
+    $response_dis = $client_dis->request('GET','json?units=imperial&origins='.$tourl_string_municipio_1.','.$tourl_string_estado_1.',México&destinations='.$tourl_string_municipio_2.','.$tourl_string_estado_2.',México&key=AIzaSyBpLNDhRQgBSlfzn7SJwZyfjyGEv-WIrrE');
+    $decode_json_dis = json_decode($response_dis->getBody());
+
+    echo "<br>Distancia: ".$decode_json_dis->rows[0]->elements[0]->distance->text."<br>";
+    echo "<br>Tiempo: ".$decode_json_dis->rows[0]->elements[0]->duration->text."<br>";
+    /*echo '<ul>
         <li>Código Postal: '.$response_cp1_object->codigo_postal.'</li>
         <li>Municipio: '.$response_cp1_object->municipio.'</li>
         <li>Estado: '.$response_cp1_object->estado.'</li>';
         foreach ($response_cp1_object->colonias as $key) {
           echo '<li>Colonias: '.$key.'</li>
           </ul>';
-        }
-        echo '<ul>
+        }*/
+        /*echo '<ul>
         <li>Código Postal: '.$response_cp2_object->codigo_postal.'</li>
         <li>Municipio: '.$response_cp2_object->municipio.'</li>
         <li>Estado: '.$response_cp2_object->estado.'</li>';
         foreach ($response_cp2_object->colonias as $key) {
           echo '<li>Colonias: '.$key.'</li>
         </ul>';
-        }
+      }*/
 
  ?>
 </body>
