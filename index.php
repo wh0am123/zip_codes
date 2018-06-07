ee1bd8d387c86fb99e5ff2402894b40d9e25518a
u809748758 <u809748758@juanpablo.tk>
2018-06-07 13:27:03 -0430

    Juan here

--- index.php
+++ index.php
@@ -4,16 +4,26 @@
 	<meta charset="UTF-8">
 	<title>Conversor</title>
 	<script src="angular.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 </head>
 <body>
-	<form action="php/process.php" method="post">
+	<form action="php/process.php" method="post" class="form" style="margin-top: 10%;">
+	<div class="row" style="display: flex;justify-content: center;">
+		<div class="col-sm-3">
 	<label for="postcode1">Código Postal #1</label>
-	<input type="text" id="etc" ng-model="cp1" name="postcode1">
-	<br>
+	<input type="text" id="etc" ng-model="cp1" name="postcode1" class="form-control">
+</div>
+	<div class="col-sm-3">
 	<label for="postcode2">Código Postal #2</label>
-	<input type="text" id="etc" ng-model="cp2" name="postcode2">
-	<br>
-	<button type="submit">Procesar</button>
+	<input type="text" id="etc" ng-model="cp2" name="postcode2" class="form-control">
+	</div>
+</div>
+<div class="row" style="display:flex;justify-content: center;margin: 1% 32%;">
+	<div class="col-sm-3">
+	<button type="submit" class="btn btn-success">Procesar</button>
+	</div>
+</div>
+
 	</form>
 </body>
 </html>
--- php/process.php
+++ php/process.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en" ng-app>
 <head>
-  <meta charset="UTF-8">
-  <title>Conversor</title>
-  <script src="angular.min.js"></script>
+	<meta charset="UTF-8">
+	<title>Conversor</title>
+	<script src="angular.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 </head>
 <body>
-  <form action="php/process.php" method="post">
-  <label for="postcode1">Código Postal #1</label>
-  <input type="text" id="etc" ng-model="cp1" name="postcode1">
-  <br>
-  <label for="postcode2">Código Postal #2</label>
-  <input type="text" id="etc" ng-model="cp2" name="postcode2">
-  <br>
-  <button type="button" name="button" onclick="location.href='../';">Volver</button>
-  </form>
+	<form action="php/process.php" method="post" class="form" style="margin-top: 10%;">
+	<div class="row" style="display: flex;justify-content: center;">
+		<div class="col-sm-3">
+	<label for="postcode1">Código Postal #1</label>
+	<input type="text" id="etc" ng-model="cp1" name="postcode1" class="form-control">
+</div>
+	<div class="col-sm-3">
+	<label for="postcode2">Código Postal #2</label>
+	<input type="text" id="etc" ng-model="cp2" name="postcode2" class="form-control">
+	</div>
+</div>
+<div class="row" style="display:flex;justify-content: center;margin: 1% 32%;">
+	<div class="col-sm-3">
+	<button type="submit" class="btn btn-success">Procesar</button>
+	</div>
+</div>
+
+	</form>
 </body>
 </html>
 
